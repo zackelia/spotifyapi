040264197a811d585f74512a5df6c40f75e09ecc
Zack Elia <github@zacharyelia.com>
2019-03-30 13:04:38 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spotifyapi",
+    version="0.0.1",
+    author="Zack Elia",
+    author_email="pypi@zacharyelia.com",
+    description="A Python wrapper to interact the Spotify web API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zackelia/spotifyapi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
